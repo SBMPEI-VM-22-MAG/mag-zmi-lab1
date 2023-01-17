ab76b7451a1e3b7a64158186165fd5bcdbce9054
kinwend17 <st.andrey17@yandex.ru>
2023-01-17 12:34:51 +0300

    some

--- src/app/shared/Components/kemeny/kemeny.component.ts
+++ src/app/shared/Components/kemeny/kemeny.component.ts
@@ -1,14 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, DoCheck } from '@angular/core';
+import { IRow } from '../../Models/base.model';
 
 @Component({
   selector: 'app-kemeny',
   templateUrl: './kemeny.component.html',
   styleUrls: ['./kemeny.component.scss'],
 })
-export class KemenyComponent implements OnInit {
+export class KemenyComponent implements OnInit, DoCheck {
+
+  @Input() tableRanks: IRow[] = [];
 
   constructor() { }
 
   ngOnInit() {}
 
+  ngDoCheck(): void {
+    console.log(this.tableRanks);
+  }
 }
